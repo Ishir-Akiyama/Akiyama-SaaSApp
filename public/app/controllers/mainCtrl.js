2c0cbd4b0f85dce96cc1237390f5ce9c430bf6be
simplekushwaha <skushwaha@ishir.com>
2016-09-14 18:18:32 +0530

    Remember Me

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -1,9 +1,10 @@
 angular.module('mainCtrl', [])
 
-.controller('mainController', function ($rootScope, $location, Auth, $window, $scope) {
+.controller('mainController', function ($rootScope, $location, Auth, $window, $scope, $cookieStore) {
     var vm = this;
-
-
+    //Get cookie
+    $scope.UserNameCookie = $cookieStore.get('CookieUserName');
+    $scope.PasswordCookie = $cookieStore.get('CookiePassword');
     // check to see if a user is logged in on every request
     $rootScope.$on('$routeChangeStart', function () {
         vm.loggedIn = Auth.isLoggedIn();
@@ -24,10 +25,20 @@ angular.module('mainCtrl', [])
     // function to handle login form
     vm.doLogin = function () {
         vm.processing = true;
-
         // clear the error
         vm.error = '';
-
+        var UserData = { UserName: vm.loginData.username, Password: vm.loginData.password }
+        var rememberMe = $scope.rememberMe;
+        if (rememberMe == true) {
+            $cookieStore.put('CookieUserName', vm.loginData.username);
+            $cookieStore.put('CookiePassword', vm.loginData.password);
+        }
+        else {
+            // Removing a cookie
+            $cookieStore.remove('CookieUserName');
+            $cookieStore.remove('CookiePassword');
+        }
+       
         Auth.login(vm.loginData.username, vm.loginData.password)
 			.success(function (data) {
 			    vm.processing = false;
