5c890c0776f07ff98f9649912971c6823027cc4a
asaxena85 <asaxena@ishir.com>
2016-09-15 19:47:54 +0530

    Image upload validation.

--- app/controller/image.server.controller.js
+++ app/controller/image.server.controller.js
@@ -23,131 +23,159 @@ exports.create = function (request, response) {
     module.exports = mongoose.model('Images_' + request.body.clientId, sch_obj);
     var Image = mongoose.model('Images_' + request.body.clientId);
 
+    try{
+        if (request.body.type.indexOf("application/vnd.openxmlformats") > -1) {
+            var bitmap = new Buffer(request.body.file.replace("data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64", ""), 'base64');
+            // write buffer to file
+            var fs = require('fs');
+            if (!fs.existsSync("Uploads")) {
+                fs.mkdirSync("Uploads");
+            }
+            var fileAddress = "Uploads/" + request.body.filename;
 
-    if (request.body.type.indexOf("application/vnd.openxmlformats") > -1) {
-        var bitmap = new Buffer(request.body.file.replace("data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64", ""), 'base64');
-        // write buffer to file
-        var fs = require('fs');
-        if (!fs.existsSync("Uploads")) {
-            fs.mkdirSync("Uploads");
+            fs.writeFileSync(fileAddress, bitmap);
+            xlsxj = require("xlsx-to-json");
+            xlsxj({
+                input: fileAddress,
+                output: fileAddress + ".json"
+            }, function (err, result) {
+                if (err) {
+                    //console.error(err);
+                    throw Error(err);
+                    response.send(err);
+                } else {
+                    var content;
+                    // First I want to read the file
+                    fs.readFile(fileAddress + '.json', function read(err, data) {
+                        if (err) {
+                            throw Error(err);
+                        }
+                        content = data;
+                        var imagesInExcel = JSON.parse(content);
+
+                        if (!imagesInExcel.name){
+                            response.json({ message: 'Failed:Incorrect file.' });
+                            return;
+                        }
+                            
+                        else if (!imagesInExcel.Image) {
+                            response.json({ message: 'Failed:Incorrect file.' });
+                            return;
+                        }
+
+                        for (var i = 0; i < imagesInExcel.length; i++) {
+                            var getType = imagesInExcel[i].Image.toString().substr(5, 20);
+                            getType = getType.substr(0, getType.indexOf(";"));
+                            var entry = new Image({
+                                name: imagesInExcel[i].name,
+                                filename: request.body.filename,
+                                type: getType,
+                                byte: imagesInExcel[i].Image,
+                                user: request.body.userid,
+                            });
+                            entry.save(function (err) {
+                                if (err) {
+                                    response.json({ message: err.message });
+                                }
+                                // return a message
+                            });
+                        }
+                        response.json({ message: 'Excel imported successfully!' });
+                    });
+                }
+            });
         }
-        var fileAddress = "Uploads/" + request.body.filename;
-
-        fs.writeFileSync(fileAddress, bitmap);
-        xlsxj = require("xlsx-to-json");
-        xlsxj({
-            input: fileAddress,
-            output: fileAddress + ".json"
-        }, function (err, result) {
-            if (err) {
-                //console.error(err);
-                //throw ex;
-                response.send(err);
-            } else {
-                var content;
-                // First I want to read the file
-                fs.readFile(fileAddress + '.json', function read(err, data) {
-                    if (err) {
-                        throw err;
-                    }
-                    content = data;
-                    var imagesInExcel = JSON.parse(content);
-
-                    console.log(imagesInExcel);
-
-                    for (var i = 0; i < imagesInExcel.length; i++) {
-                        var getType = imagesInExcel[i].Image.toString().substr(5, 20);
-                        getType = getType.substr(0, getType.indexOf(";"));
-                        var entry = new Image({
-                            name: imagesInExcel[i].name,
-                            filename: request.body.filename,
-                            type: getType,
-                            byte: imagesInExcel[i].Image,
-                            user: request.body.userid,
-                        });
-                        entry.save(function (err) {
-                            if (err) { }
-                            // return a message
-                        });
-                    }
-                    response.json({ message: 'Excel imported successfully!' });
-                });
+        else if (request.body.type.indexOf("application/vnd.ms-excel") > -1) {
+            var bitmap = new Buffer(request.body.file.replace("data:application/vnd.ms-excel;base64", ""), 'base64');
+            // write buffer to file
+            var fs = require('fs');
+            if (!fs.existsSync("Uploads")) {
+                fs.mkdirSync("Uploads");
             }
-        });
-    }
-    else if (request.body.type.indexOf("application/vnd.ms-excel") > -1) {
-        var bitmap = new Buffer(request.body.file.replace("data:application/vnd.ms-excel;base64", ""), 'base64');
-        // write buffer to file
-        var fs = require('fs');
-        if (!fs.existsSync("Uploads")) {
-            fs.mkdirSync("Uploads");
+            var fileAddress = "Uploads/" + request.body.filename;
+            fs.writeFileSync(fileAddress, bitmap);
+
+            var Converter = require("csvtojson").Converter;
+            var converter = new Converter({});
+            converter.fromFile(fileAddress, function (err, result) {
+
+                if (err) {
+
+                    console.error(err);
+                    throw ex;
+                } else {
+
+                    fs.writeFileSync(fileAddress + '.json', JSON.stringify(result));
+
+                    var content;
+                    // First I want to read the file
+                    fs.readFile(fileAddress + '.json', function read(err, data) {
+                        if (err) {
+                            throw err;
+                        }
+                        content = data;
+
+                        var imagesInExcel = JSON.parse(content);
+                        if (!imagesInExcel.name) {
+                            response.json({ message: 'Failed:Incorrect file.' });
+                            return;
+                        }
+
+                        else if (!imagesInExcel.Image) {
+                            response.json({ message: 'Failed:Incorrect file.' });
+                            return;
+                        }
+                        for (var i = 0; i < imagesInExcel.length; i++) {
+                            var getType = imagesInExcel[i].Image.toString().substr(5, 20);
+                            getType = getType.substr(0, getType.indexOf(";"));
+                            var entry = new Image({
+                                name: imagesInExcel[i].name,
+                                filename: request.body.filename,
+                                type: getType,
+                                byte: imagesInExcel[i].Image,
+                                user: request.body.userid,
+                            });
+                            entry.save(function (err) {
+                                if (err) {
+                                    response.json({ message: err.message });
+                                }
+                                // return a message
+                            });
+                        }
+                        response.json({ message: 'CSV imported successfully!' });
+                    });
+                }
+            });
         }
-        var fileAddress = "Uploads/" + request.body.filename;
-        fs.writeFileSync(fileAddress, bitmap);
+        else if (request.body.type.indexOf("image") > -1) {
 
-        var Converter = require("csvtojson").Converter;
-        var converter = new Converter({});
-        converter.fromFile(fileAddress, function (err, result) {
+            var entry = new Image({
+                name: request.body.name,
+                filename: request.body.filename,
+                type: request.body.type,
+                byte: request.body.file,
+                user: request.body.userid
+            });
+            entry.save(function (err) {
 
-            if (err) {
-
-                console.error(err);
-                throw ex;
-            } else {
-
-                fs.writeFileSync(fileAddress + '.json', JSON.stringify(result));
-
-                var content;
-                // First I want to read the file
-                fs.readFile(fileAddress + '.json', function read(err, data) {
-                    if (err) {
-                        throw err;
-                    }
-                    content = data;
-
-                    var imagesInExcel = JSON.parse(content);
-
-                    for (var i = 0; i < imagesInExcel.length; i++) {
-                        var getType = imagesInExcel[i].Image.toString().substr(5, 20);
-                        getType = getType.substr(0, getType.indexOf(";"));
-                        var entry = new Image({
-                            name: imagesInExcel[i].name,
-                            filename: request.body.filename,
-                            type: getType,
-                            byte: imagesInExcel[i].Image,
-                            user: request.body.userid,
-                        });
-                        entry.save(function (err) {
-                            if (err) { }
-                            // return a message
-                        });
-                    }
-                    response.json({ message: 'Csv imported successfully!' });
-                });
-            }
-        });
+                if (err) {
+                    // duplicate entry
+                    //if (err.code == 11000) 
+                    //    return res.json({ success: false, message: 'A user with that username already exists. '});
+                    //else
+                    response.json({ message: err.message });
+                }
+                // return a message
+                response.json({ message: 'Image uploaded successfully!' });
+            });
+        }
+        else {
+            response.json({ message: 'Failed: Inavlid file.' });
+        }
     }
-    else {
+    catch (e) {
 
-        var entry = new Image({
-            name: request.body.name,
-            filename: request.body.filename,
-            type: request.body.type,
-            byte: request.body.file,
-            user: request.body.userid
-        });
-        entry.save(function (err) {
-
-            if (err) {
-                // duplicate entry
-                //if (err.code == 11000) 
-                //    return res.json({ success: false, message: 'A user with that username already exists. '});
-                //else
-                return response.send(err);
-            }
-            // return a message
-            response.json({ message: 'Image created successfully!' });
-        });
+        response.json({ message: 'Failed: Something is wrong with the file, please validate the file and try again.' });
     }
     //console.log(response);
 };
